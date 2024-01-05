44ed6b0cf3188d8d2e6711d76b1475bf5e5e6a49
Jacob Bieker <jacob@bieker.tech>
2024-01-05 15:28:33 +0000

    Update requirements
    Add note on ISD being slow through Monetio, can be a lot faster through S3

--- planetary_datasets/raw/noaa/integrated_surface_database.py
+++ planetary_datasets/raw/noaa/integrated_surface_database.py
@@ -3,12 +3,22 @@ import pandas as pd
 import xarray as xr
 from monetio.obs import ish
 import datetime as dt
+import s3fs
 
 
-def get_isd_observations(start_time: dt.datetime, end_time: dt.datetime, resample: bool = False) -> xr.Dataset:
-    """Fetch Aeronet data for a given day."""
-    dates = pd.date_range(start=start_time, end=end_time, freq='H')
+
+def get_isd_observations_monetio(start_time: dt.datetime, end_time: dt.datetime, resample: bool = False) -> xr.Dataset:
+    """Fetch ISD observations data for a given day using Monetio (Can be quite slow)."""
+    dates = pd.date_range(start=start_time, end=end_time, freq='D')
     df = ish.add_data(dates, resample=resample, window="1H")
+    ds = df.to_xarray()
+    ds = ds.rename_vars({k: k.replace(" ", "_") for k in ds.data_vars.keys()})
+    return ds
+
+def get_isd_observations(start_time: dt.datetime, end_time: dt.datetime) -> xr.Dataset:
+    """Fetch ISD observations data for a given day from AWS."""
+    dates = pd.date_range(start=start_time, end=end_time, freq='D')
+    df = ish.add_data(dates, resample=False, window="1H")
     ds = df.to_xarray()
     ds = ds.rename_vars({k: k.replace(" ", "_") for k in ds.data_vars.keys()})
     return ds
