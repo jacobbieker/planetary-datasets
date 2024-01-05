3013aaa9264ddca7e1e596147412ca6abcf2e2c1
Jacob Bieker <jacob@bieker.tech>
2024-01-05 11:44:22 +0000

    Add resampling of ISD and refactor

--- planetary_datasets/raw/noaa/integrated_surface_database.py
+++ planetary_datasets/raw/noaa/integrated_surface_database.py
@@ -5,10 +5,10 @@ from monetio.obs import ish
 import datetime as dt
 
 
-def get_isd_observations(start_time: dt.datetime, end_time: dt.datetime) -> xr.Dataset:
+def get_isd_observations(start_time: dt.datetime, end_time: dt.datetime, resample: bool = False) -> xr.Dataset:
     """Fetch Aeronet data for a given day."""
     dates = pd.date_range(start=start_time, end=end_time, freq='H')
-    df = ish.add_data(dates, resample=False)
+    df = ish.add_data(dates, resample=resample, window="1H")
     ds = df.to_xarray()
     ds = ds.rename_vars({k: k.replace(" ", "_") for k in ds.data_vars.keys()})
     return ds
