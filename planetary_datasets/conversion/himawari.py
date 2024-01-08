f9293c0b90f8e9bcd33324c8785390d0851a72ca
Jacob Bieker <jacob@bieker.tech>
2024-01-08 08:11:30 +0000

    Linting

--- planetary_datasets/conversion/himawari.py
+++ planetary_datasets/conversion/himawari.py
@@ -1,37 +1,35 @@
 import datetime as dt
 import fsspec
 import ujson
-import os, zipfile
+import zipfile
 import pandas as pd
 import random
 import shutil
 from huggingface_hub import HfApi
-import xarray as xr
 import os
 
 from kerchunk.hdf import SingleHdf5ToZarr
-# C should be channels 0 to 15 I think, or 1 to 16, zero padded to 2 digits
 
 
 def get_himawari_kerchunk(time: dt.datetime, raw_location: str, output_location: str):
     """
     Generate a Kerchunk file from a Himawari file
     """
-    so = dict(mode='rb', anon=True, default_fill_cache=False, default_cache_type='first')
+    so = dict(mode="rb", anon=True, default_fill_cache=False, default_cache_type="first")
     fs_read = fsspec.filesystem("s3", anon=True, skip_instance_cache=True)
     fs2 = fsspec.filesystem("")
     # Do it per day vs per minute, only want ones that are every 10 minutes
     raw_location = f"{raw_location}/AHI-L2-FLDK-ISatSS/{time.strftime('%Y')}/{time.strftime('%m')}/{time.strftime('%d')}/*/*.nc"
     files = fs_read.glob(raw_location)
     # Remove any files where the minute of day is not a multiple of 10
-    files = ['s3://' + f for f in files if int(f.split("/")[-2]) % 10 == 0]
+    files = ["s3://" + f for f in files if int(f.split("/")[-2]) % 10 == 0]
     # Do the same for the cloud and wind vectors
     raw_location = f"{raw_location}/AHI-L2-FLDK-Clouds/{time.strftime('%Y')}/{time.strftime('%m')}/{time.strftime('%d')}/*/*.nc"
     files_cloud = fs_read.glob(raw_location)
-    files_cloud = ['s3://' + f for f in files_cloud if int(f.split("/")[-2]) % 10 == 0]
+    files_cloud = ["s3://" + f for f in files_cloud if int(f.split("/")[-2]) % 10 == 0]
     raw_location = f"{raw_location}/AHI-L2-FLDK-Winds/{time.strftime('%Y')}/{time.strftime('%m')}/{time.strftime('%d')}/*/*.nc"
     files_wind = fs_read.glob(raw_location)
-    files_wind = ['s3://' + f for f in files_wind if int(f.split("/")[-2]) % 10 == 0]
+    files_wind = ["s3://" + f for f in files_wind if int(f.split("/")[-2]) % 10 == 0]
     # Combine the files
     files = sorted(files + files_cloud + files_wind)
     for file_url in files:
@@ -39,13 +37,13 @@ def get_himawari_kerchunk(time: dt.datetime, raw_location: str, output_location:
             h5chunks = SingleHdf5ToZarr(infile, file_url, inline_threshold=300)
             outfile = file_url.replace("nc", "json").split("/")[-1]
             outfile = os.path.join(output_location, outfile)
-            with fs2.open(outfile, 'wb') as f:
+            with fs2.open(outfile, "wb") as f:
                 f.write(ujson.dumps(h5chunks.translate()).encode())
 
 
 def zip_jsons(time, output_folder):
     zip_name = f"{time.strftime('%Y%m%d')}.zip"
-    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_ref:
+    with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zip_ref:
         for folder_name, subfolders, filenames in os.walk(output_folder):
             for filename in filenames:
                 print(filename)
@@ -95,7 +93,9 @@ if __name__ == "__main__":
     start_idx = random.randint(0, len(date_range))
     for day in date_range[start_idx:]:
         os.mkdir(args.output_location)
-        get_himawari_kerchunk(day, raw_location=args.raw_location, output_location=args.output_location)
+        get_himawari_kerchunk(
+            day, raw_location=args.raw_location, output_location=args.output_location
+        )
         zip_name = zip_jsons(day, args.output_location)
         if args.upload_to_hf:
             upload_to_hf(zip_name, args.hf_token, repo_id=repo_id)
