dc2bf77e91fea521fa35cb467a2cd06516d325ae
Jacob Bieker <jacob@bieker.tech>
2024-01-08 15:07:02 +0000

    Update getting multi-resolution GOES imagery

--- planetary_datasets/processed/goes.py
+++ planetary_datasets/processed/goes.py
@@ -12,9 +12,9 @@ def get_goes_image(
     start_datetime: dt.datetime,
     end_datetime: dt.datetime,
     image_type: str = "FULL DISK",
-    bands: Optional[list[int]] = None,
+    bands_to_use: Optional[list[int]] = None,
     full_resolution: bool = False,
-) -> Union[xr.Dataset, None]:
+) -> Union[list[xr.Dataset], None]:
     """
     Get the GOES images for the given time period
 
@@ -22,7 +22,7 @@ def get_goes_image(
         start_datetime: datetime to start getting images from
         end_datetime: End datetime to get images from
         image_type: Which GOES image type to get, either 'CONUS' or 'FULL DISK'
-        bands: Which bands to get, if None, all bands are included
+        bands_to_use: Which bands to get, if None, all bands are included
         full_resolution: Whether to get the full resolution images,
             which are variable in band, or the fixed resolution images, which are 2km
 
@@ -33,6 +33,8 @@ def get_goes_image(
     assert image_type in ["CONUS", "FULL DISK"], ValueError(
         f"Image type {image_type=} not recognized"
     )
+    if bands_to_use is None:
+        bands_to_use = list(range(1, 17)) # 1-16
     catalog = pystac_client.Client.open(
         "https://planetarycomputer.microsoft.com/api/stac/v1",
         modifier=planetary_computer.sign_inplace,
@@ -45,28 +47,51 @@ def get_goes_image(
     timesteps = []
     for i, item in enumerate(search.items()):
         bands = []
-        for idx in range(1, 7):
-            bands.append(f"C{idx:02d}_2km")
-        common_names = [
-            item.assets[band].extra_fields["eo:bands"][0]["common_name"]
-            for band in bands
-            if "common_name" in item.assets[band].extra_fields["eo:bands"][0]
-        ]
-        ds = xr.concat(
-            [rioxarray.open_rasterio(item.assets[band].href) for band in bands], dim="band"
-        ).assign_coords(band=common_names)
-        # Add created date as a coordinate
-        ds = ds.assign_coords(
-            {"time": dt.datetime.strptime(ds.attrs["date_created"], "%Y-%m-%dT%H:%M:%S.%fZ")}
-        )
-        timesteps.append(ds)
-    ds = xr.concat(timesteps, dim="time")
-    ds = ds.sortby("time").transpose("time", "band", "x", "y")
-    # Add lat/lon coordinates
-    ds = calc_latlon(ds)
-    # Add CRS to main attributes
-    ds.attrs["crs"] = ds.rio.crs
-    return ds
+        for idx in bands_to_use:
+            if full_resolution:
+                if idx in [1, 3, 5]:
+                    bands.append(f"C{idx:02d}_1km")
+                elif idx in [2,]:
+                    bands.append(f"C{idx:02d}_0.5km")
+                else:
+                    bands.append(f"C{idx:02d}_2km")
+            else:
+                bands.append(f"C{idx:02d}_2km")
+
+        if full_resolution:
+            ds_20 = xr.concat(
+                [rioxarray.open_rasterio(item.assets[band].href) for band in bands if "_2km"], dim="band"
+            )
+            ds_05 = xr.concat(
+                [rioxarray.open_rasterio(item.assets[band].href) for band in bands if "0.5km" in band], dim="band"
+            )
+            # Load the 1km bands
+            ds_1 = xr.concat(
+                [rioxarray.open_rasterio(item.assets[band].href) for band in bands if "_1km" in band], dim="band"
+            )
+            dses = [ds_20, ds_05, ds_1]
+        else:
+            ds_20 = xr.concat(
+                [rioxarray.open_rasterio(item.assets[band].href) for band in bands], dim="band"
+            )
+            dses = [ds_20]
+        for idx, ds in enumerate(dses):
+            # Add created date as a coordinate
+            dses[idx] = ds.assign_coords(
+                {"time": dt.datetime.strptime(ds.attrs["date_created"], "%Y-%m-%dT%H:%M:%S.%fZ")}
+            )
+        timesteps.append(dses)
+    # Concatentate along the same index in each sublist
+    dses = [xr.concat([timestep[idx] for timestep in timesteps], dim="time") for idx in range(len(timesteps[0]))]
+    for idx, ds in enumerate(dses):
+        # Add the band names as coordinates
+        ds = ds.sortby("time").transpose("time", "band", "x", "y")
+        # Add lat/lon coordinates
+        ds = calc_latlon(ds)
+        # Add CRS to main attributes
+        ds.attrs["crs"] = ds.rio.crs
+        dses[idx] = ds
+    return dses
 
 
 def calc_latlon(ds: xr.Dataset) -> xr.Dataset:
