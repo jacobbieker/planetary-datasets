dc2f1a6dfe5e2fa5826fab74eeb33b6cad3630e6
Jacob Prince-Bieker <jacob@bieker.tech>
2025-02-09 09:30:43 +0000

    Add downloading ASOS houry and 1 minutely data

--- planetary_datasets/provider/meteostat/observations.py
+++ planetary_datasets/provider/meteostat/observations.py
@@ -2,20 +2,21 @@ import os
 import json
 import fsspec
 from pathlib import Path
+from aiohttp.client_exceptions import ClientPayloadError
 
 
 def download_file(remote_path, local_path):
     full_local_path = local_path
     print(remote_path)
     if full_local_path.exists():
-        return full_local_path
+        return
     with fsspec.open(remote_path, "rb") as infile:
         with fsspec.open(str(full_local_path), "wb") as outfile:
             outfile.write(infile.read())
 
 
 def download_meteostat():
-    with fsspec.open("https://bulk.meteostat.net/v2/stations/full.json.gz", compression="infer") as f:
+    with fsspec.open("https://bulk.meteostat.net/v2/stations/lite.json.gz", compression="infer") as f:
         weather_stations = json.load(f)
     station_ids = [s['id'] for s in weather_stations]
     for station_id in station_ids:
@@ -26,5 +27,25 @@ def download_meteostat():
             download_file(remote_path, local_path)
         except FileNotFoundError:
             continue
+        except ClientPayloadError:
+            # Try once more
+            try:
+                download_file(remote_path, local_path)
+            except:
+                continue
+    for station_id in station_ids:
+        remote_path = f"https://bulk.meteostat.net/v2/hourly/{station_id}.map.csv.gz"
+        # local_path = Path(os.path.basename(remote_path))
+        local_path = Path(f"data/{station_id}.csv.gz")
+        try:
+            download_file(remote_path, local_path)
+        except FileNotFoundError:
+            continue
+        except ClientPayloadError:
+            # Try once more
+            try:
+                download_file(remote_path, local_path)
+            except:
+                continue
 
 download_meteostat()
