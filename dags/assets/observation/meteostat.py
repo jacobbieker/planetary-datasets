df0714e887a310e26daad8656f0d7af026832e86
Jacob Prince-Bieker <jacob@bieker.tech>
2025-07-02 14:01:44 +0100

    Add more checks

--- dags/assets/observation/meteostat.py
+++ dags/assets/observation/meteostat.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+import pandas as pd
 
 import fsspec
 from aiohttp.client_exceptions import ClientPayloadError
@@ -7,7 +8,6 @@ from aiohttp.client_exceptions import ClientPayloadError
 
 def download_file(remote_path, local_path):
     full_local_path = local_path
-    print(remote_path)
     if full_local_path.exists():
         return
     with fsspec.open(remote_path, "rb") as infile:
@@ -15,15 +15,30 @@ def download_file(remote_path, local_path):
             outfile.write(infile.read())
 
 import tqdm
+import os
 def download_meteostat():
     with fsspec.open("https://bulk.meteostat.net/v2/stations/lite.json.gz", compression="infer") as f:
         weather_stations = json.load(f)
     station_ids = [s['id'] for s in weather_stations]
+    inventory = [s['inventory'] for s in weather_stations]
+    starts = [s['hourly']['start'] for s in inventory]
+    ends = [s['hourly']['end'] for s in inventory]
+
     for year in range(2024, 1999, -1):
-        for station_id in tqdm.tqdm(station_ids, total=len(station_ids)):
+        for i, station_id in tqdm.tqdm(enumerate(station_ids), total=len(station_ids)):
+            try:
+                starts[i] = pd.to_datetime(starts[i]).strftime("%Y-%m-%d")
+                ends[i] = pd.to_datetime(ends[i]).strftime("%Y-%m-%d")
+            except AttributeError:
+                # If the date conversion fails, skip this station
+                continue
+            if starts[i] > pd.to_datetime(f"{year}-12-31").strftime("%Y-%m-%d") or ends[i] < pd.to_datetime(f"{year}-01-01").strftime("%Y-%m-%d"):
+                continue
             remote_path = f"https://data.meteostat.net/hourly/{year}/{station_id}.csv.gz"
             #local_path = Path(os.path.basename(remote_path))
             local_path = Path(f"/Users/jacob/Development/meteostat_data/{year}_{station_id}.csv.gz")
+            if os.path.exists(local_path):
+                continue
             try:
                 download_file(remote_path, local_path)
             except FileNotFoundError:
