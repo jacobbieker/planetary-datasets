8e7071839ebfe6a5a9ee696cfd1d5b5574062962
Jacob Prince-Bieker <jacob@bieker.tech>
2025-07-03 14:37:09 +0100

    Fix drifter URL

--- dags/assets/observation/drifters.py
+++ dags/assets/observation/drifters.py
@@ -10,7 +10,7 @@ for date in date_range[::-1]:
     end_month = (date + pd.DateOffset(months=3)).strftime("%Y-%m-%d")
     time_part_of_url = f"%22&time%3E={start_month}T00%3A00%3A00Z&time%3C={end_month}T23%3A59%3A59Z"
     ships = "&platform_type=%22SHIPS%20(GENERIC)"
-    drifters = "&platform_type=%22SHIPS%20(GENERIC)"
+    drifters = "&platform_type=%22DRIFTING%20BUOYS%20(GENERIC)"
     base_url = "https://erddap.aoml.noaa.gov/gdp/erddap/tabledap/OSMC_RealTime.nc?platform_type%2Ctime%2Clatitude%2Clongitude%2Csst%2Catmp%2Cprecip%2Csss%2Cslp%2Cwindspd%2Cwinddir%2Cdewpoint%2Cuo%2Cvo%2Cwo%2Crainfall_rate%2Chur"
     ship_url = base_url+ships + time_part_of_url
     drifter_url = base_url+drifters+time_part_of_url
