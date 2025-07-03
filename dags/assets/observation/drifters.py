2c516e1190a25e3a878aee167c6cd516958eeadb
Jacob Prince-Bieker <jacob@bieker.tech>
2025-07-03 14:48:19 +0100

    Add moored buoys as well

--- dags/assets/observation/drifters.py
+++ dags/assets/observation/drifters.py
@@ -11,9 +11,11 @@ for date in date_range[::-1]:
     time_part_of_url = f"%22&time%3E={start_month}T00%3A00%3A00Z&time%3C={end_month}T23%3A59%3A59Z"
     ships = "&platform_type=%22SHIPS%20(GENERIC)"
     drifters = "&platform_type=%22DRIFTING%20BUOYS%20(GENERIC)"
+    moored = "&platform_type=%22MOORED%20BUOYS%20(GENERIC)"
     base_url = "https://erddap.aoml.noaa.gov/gdp/erddap/tabledap/OSMC_RealTime.nc?platform_type%2Ctime%2Clatitude%2Clongitude%2Csst%2Catmp%2Cprecip%2Csss%2Cslp%2Cwindspd%2Cwinddir%2Cdewpoint%2Cuo%2Cvo%2Cwo%2Crainfall_rate%2Chur"
     ship_url = base_url+ships + time_part_of_url
     drifter_url = base_url+drifters+time_part_of_url
+    moored_url = base_url+moored+time_part_of_url
 
     if os.path.exists(f"Ships/{date.strftime('%Y-%m')}_ships.nc"):
         try:
@@ -51,6 +53,24 @@ for date in date_range[::-1]:
         drifter_response = requests.get(drifter_url)
         print(f"Got Drifter data for {start_month} - {end_month}")
         with open(f"Drifters/{date.strftime('%Y-%m')}_drifters.nc", "wb") as f:
+            f.write(drifter_response.content)
+
+    if os.path.exists(f"Moored/{date.strftime('%Y-%m')}_moored.nc"):
+        try:
+            xr.open_dataset(f"Moored/{date.strftime('%Y-%m')}_moored.nc").load()  # If this succeeds, it was successful
+        except:
+            # Do for drifters
+            print(f"Requesting Moored data for {start_month} - {end_month}")
+            drifter_response = requests.get(moored_url)
+            print(f"Got Moored data for {start_month} - {end_month}")
+            with open(f"Moored/{date.strftime('%Y-%m')}_moored.nc", "wb") as f:
+                f.write(drifter_response.content)
+    else:
+        # Do for drifters
+        print(f"Requesting Moored data for {start_month} - {end_month}")
+        drifter_response = requests.get(moored_url)
+        print(f"Got Moored data for {start_month} - {end_month}")
+        with open(f"Moored/{date.strftime('%Y-%m')}_moored.nc", "wb") as f:
             f.write(drifter_response.content)
 
 
