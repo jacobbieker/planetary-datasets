c8f097b3c00e292ead2712868f48d1e327aa98e0
Jacob Prince-Bieker <jacob@bieker.tech>
2025-07-24 11:44:44 +0100

    Do it chunked

--- dags/assets/virtual_goes.py
+++ dags/assets/virtual_goes.py
@@ -7,36 +7,16 @@ from virtualizarr import open_virtual_dataset, open_virtual_mfdataset
 from virtualizarr.parsers import HDFParser
 from virtualizarr.registry import ObjectStoreRegistry
 import icechunk
-from icechunk.storage import local_filesystem_storage
 import s3fs
+from concurrent.futures import ThreadPoolExecutor
 
 fs = s3fs.S3FileSystem(anon=True, client_kwargs={}, asynchronous=False)
 bucket = "s3://noaa-goes19"
 path = "ABI-L2-MCMIPF/2025/002/01/OR_ABI-L2-MCMIPF-M6_G19_s20250020100206_e20250020109526_c20250020110001.nc"
+#path = "ABI-L2-MCMIPF/2025/002/01/OR_ABI-L2-MCMIPF-M6_G18_s20250020100217_e20250020109532_c20250020110021.nc"
 url = f"{bucket}/{path}"
 store = from_url(bucket, skip_signature=True)
 registry = ObjectStoreRegistry({bucket: store})
-storage = local_filesystem_storage("goes19_mcmipf.icechunk")
-repo = icechunk.Repository.open_or_create(storage)
-files = []
-for day_of_year in range(1, 2):
-    # Format the day of year as a three-digit string
-    day_str = f"{day_of_year:03d}"
-    for hour in range(0, 1):
-        # Format the hour as a two-digit string
-        hour_str = f"{hour:02d}"
-        # Construct the path for the current day and hour
-        path = f"ABI-L2-MCMIPF/2025/{day_str}/{hour_str}/"
-        # List files in the directory
-        try:
-            new_files = fs.ls(f"{bucket}/{path}")
-        except FileNotFoundError:
-            print(f"Directory {path} not found, skipping.")
-            continue
-        files.extend(new_files)
-print(files)
-files = ["s3://" + f for f in files]
-
 
 import warnings
 
@@ -63,26 +43,47 @@ def preprocess(vds: xr.Dataset) -> xr.Dataset:
     vds = vds.rename({"t": "time"})
     return vds
 
+storage = icechunk.local_filesystem_storage("/raid/goes19_mcmipf.icechunk")
+repo = icechunk.Repository.open_or_create(storage)
 
-vds = open_virtual_mfdataset(
-    files,
-    parser=parser,
-    registry=registry,
-    loadable_variables=["y", "x", "t", "number_of_time_bounds", "number_of_image_bounds", "band"],
-    decode_times=True,
-    combine="nested",
-    concat_dim="time",
-    compat="override",
-    parallel="dask",
-    data_vars="minimal",
-    coords="minimal",
-    preprocess=preprocess,
-).sortby("time")
-print(vds)
-# By default, local virtual references and public remote virtual references can be read without extra configuration.
-session = repo.writable_session("main")
+for day_of_year in range(1, 205):
+    # Format the day of year as a three-digit string
+    day_str = f"{day_of_year:03d}"
+    for hour in range(0, 24):
+        # Format the hour as a two-digit string
+        hour_str = f"{hour:02d}"
+        # Construct the path for the current day and hour
+        path = f"ABI-L2-MCMIPF/2025/{day_str}/{hour_str}/"
+        # List files in the directory
+        try:
+            files = fs.ls(f"{bucket}/{path}")
+            vds = open_virtual_mfdataset(
+                ["s3://" + f for f in files],
+                parser=parser,
+                registry=registry,
+                loadable_variables=["y", "x", "t", "number_of_time_bounds", "number_of_image_bounds", "band"],
+                decode_times=True,
+                combine="nested",
+                concat_dim="time",
+                data_vars="minimal",
+                coords="minimal",
+                compat="override",
+                parallel=ThreadPoolExecutor,
+                preprocess=preprocess,
+            )
+            print(vds)
 
-# write the virtual dataset to the session with the IcechunkStore
-vds.vz.to_icechunk(session.store)
-snapshot_id = session.commit("Wrote first dataset")
-print(snapshot_id)
+            # By default, local virtual references and public remote virtual references can be read without extra configuration.
+            session = repo.writable_session("main")
+
+            # write the virtual dataset to the session with the IcechunkStore
+            if day_of_year == 1 and hour == 0:
+                # If this is the first dataset, write it without appending
+                vds.vz.to_icechunk(session.store)
+            else:
+                vds.vz.to_icechunk(session.store, append_dim="time")
+            snapshot_id = session.commit(f"Wrote {day_str} {hour_str}")
+            print(snapshot_id)
+        except FileNotFoundError:
+            print(f"Directory {path} not found, skipping.")
+            continue
